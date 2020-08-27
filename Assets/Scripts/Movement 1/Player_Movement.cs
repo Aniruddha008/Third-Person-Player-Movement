48d74294052cd3dee812e82493aa272ba4766fe6
aniruddha_tarodekar <tarodekaraniruddha@gmail.com>
2020-08-27 12:15:47 -0400

    Rotation completed with mouse input

--- Assets/Scripts/Movement 1/Player_Movement.cs
+++ Assets/Scripts/Movement 1/Player_Movement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player_Movement : MonoBehaviour
 {
     [SerializeField] float speed = 4f;
+    [SerializeField] float rotationSpeed = 2f;
 
     CharacterController controller;
     
@@ -17,7 +18,11 @@ public class Player_Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector2 mouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        transform.Rotate(Vector3.up, mouseInput.x * rotationSpeed);
+
         Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        controller.Move(input * speed * Time.deltaTime);
+        //controller.Move(input * speed * Time.deltaTime);
+        controller.Move(transform.TransformDirection(input * speed * Time.deltaTime));
     }
 }
