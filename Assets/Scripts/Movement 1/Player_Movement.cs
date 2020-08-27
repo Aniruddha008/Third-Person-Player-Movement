be2eda455f8f0d66ca08912c567ff7def2c14023
aniruddha_tarodekar <tarodekaraniruddha@gmail.com>
2020-08-27 11:41:29 -0400

    added character controllerand changes to script

--- Assets/Scripts/Movement 1/Player_Movement.cs
+++ Assets/Scripts/Movement 1/Player_Movement.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class Player_Movement : MonoBehaviour
 {
     [SerializeField] float speed = 4f;
+
+    CharacterController controller;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        transform.position += input * speed *Time.deltaTime;
+        controller.Move(input * speed * Time.deltaTime);
     }
 }
