d35139c9088e42ce13f91f414a42fbf331e84e4d
aniruddha_tarodekar <tarodekaraniruddha@gmail.com>
2020-08-26 23:34:25 -0400

    basic movement wsad added

---
+++ Assets/Scripts/Movement 1/Player_Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    [SerializeField] float speed = 4f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        transform.position += input * speed *Time.deltaTime;
+    }
+}
