79084b0a4d8a34598243e88d85152732af494e76
aniruddha_tarodekar <tarodekaraniruddha@gmail.com>
2020-08-29 15:28:48 -0400

    Jump added

--- Assets/Scripts/Movement 1/Player_Movement.cs
+++ Assets/Scripts/Movement 1/Player_Movement.cs
@@ -10,15 +10,22 @@ public class Player_Movement : MonoBehaviour
     [SerializeField] float minHeadRotation = -80.0f;
 
     [SerializeField] float currentHeadRotation = 0;
+    [SerializeField] float yVelocity = 0;
+    [SerializeField] float jumpSpeed = 15.0f;
+    [SerializeField] float gravity = 30.0f;
 
     [SerializeField] Transform head;
 
     CharacterController controller;
+    Rigidbody rigidBody;
+    
     
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        rigidBody = GetComponent<Rigidbody>();
+       
     }
 
     // Update is called once per frame
@@ -34,6 +41,14 @@ public class Player_Movement : MonoBehaviour
         
         Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         //controller.Move(input * speed * Time.deltaTime);
-        controller.Move(transform.TransformDirection(input * speed * Time.deltaTime));
+        yVelocity -= gravity * Time.deltaTime;
+        controller.Move(transform.TransformDirection(input * speed * Time.deltaTime + yVelocity * Vector3.up * Time.deltaTime));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+             yVelocity = jumpSpeed;
+        }
+
+        
     }
 }
